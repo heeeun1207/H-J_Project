0744cccd4ac797bc3539904577afa08586fd5de0
jungheeeun <jungheeeun272@gmail.com>
2023-07-13 15:58:39 +0900

    server.js  'Hello, Express!'

---
+++ my-app/server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, Express!');
+});
+
+app.listen(5001, () => {
+  console.log('Express server is running on port 5001');
+});
+
