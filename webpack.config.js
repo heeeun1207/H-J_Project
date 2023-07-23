0d56661ad3d54b02a17e37aa8f38bd348ad6d56e
jungjieun <je_eun2735@naver.com>
2023-07-23 15:44:50 +0900

    output

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 import ESLintPlugin from "eslint-webpack-plugin";
+const path = require("path");
 
 module.exports = {
   mode: "production",
-  entry: "./index.ts",
+  entry: "./src/index.ts",
   module: {
     rules: [
       {
@@ -15,5 +16,13 @@ module.exports = {
   resolve: {
     extensions: [".ts", ".js"],
   },
-  plugins: [new ESLintPlugin({ extensions: ["ts", "js"] })],
+  plugins: [new ESLintPlugin({ 
+    fix: true, //자동수정 추가
+    extensions: ["ts", "js"] 
+  })],
+
+  output: {
+    filename: "index.bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  }
 };
