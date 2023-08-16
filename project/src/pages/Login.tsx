d7848362440c8648102eff8cb5995ccb9939cf3d
jungjieun <je_eun2735@naver.com>
2023-08-16 19:09:36 +0900

    button이 아니라 select 로 변경해야할 것 같음.

--- project/src/pages/Login.tsx
+++ project/src/pages/Login.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import '../styles/login.css';
 import logoImage from '../assets/HJ_logo.png';
+import { useLanguageToggle } from '../hooks/useLanguageToggle.js';
 
 function Login() {
+  const { language, toggleLanguage } = useLanguageToggle();
   return (
     <div className="login-container">
       <header className="logo-box">
         <img src={logoImage} alt='logo'></img>
-        
+        <button onClick={toggleLanguage}>언어선택</button>
+        {language ==='ko' ? '한국어' : 'English'}
         <button>로그인</button>
       </header>
     </div>
