f6214d15fb7179a317a3aa0bc13dd646c20dc397
jungjieun <je_eun2735@naver.com>
2023-08-16 20:29:42 +0900

    option english 주석처리

--- project/src/pages/Login.tsx
+++ project/src/pages/Login.tsx
@@ -5,14 +5,27 @@ import { useLanguageToggle } from '../hooks/useLanguageToggle.js';
 
 function Login() {
   const { language, toggleLanguage } = useLanguageToggle();
+
+  const handleLanguageChange = (event) => {
+    const newLanguage = event.target.value;
+    toggleLanguage(newLanguage);
+  };
+
   return (
     <div className="login-container">
       <header className="logo-box">
         <img src={logoImage} alt='logo'></img>
-        <button onClick={toggleLanguage}>언어선택</button>
-        {language ==='ko' ? '한국어' : 'English'}
-        <button>로그인</button>
+        <select value={language} onChange={handleLanguageChange}>
+          <option value="ko">한국어</option>
+          {/* <option value="en">English</option> */}
+        </select>
       </header>
+      <main>
+        <p>찐플에 오신걸 환영합니다!</p>
+        <h1>넷플릭스 추천작을 한눈에</h1>
+        <button>추천작 보러가기</button>
+
+      </main>
     </div>
   );
 }
