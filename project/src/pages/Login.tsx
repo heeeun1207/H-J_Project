340de10163a14b5560ac809d501bfa984abfa413
jungjieun <je_eun2735@naver.com>
2023-08-16 20:53:19 +0900

    useNavigate로 페이지 이동

--- project/src/pages/Login.tsx
+++ project/src/pages/Login.tsx
@@ -2,15 +2,20 @@ import React from 'react';
 import '../styles/login.css';
 import logoImage from '../assets/HJ_logo.png';
 import { useLanguageToggle } from '../hooks/useLanguageToggle.js';
+import {useNavigate} from 'react-router-dom';
 
 function Login() {
   const { language, toggleLanguage } = useLanguageToggle();
+  const navigate = useNavigate();
 
   const handleLanguageChange = (event) => {
     const newLanguage = event.target.value;
     toggleLanguage(newLanguage);
   };
 
+  const handleBtnClick = () => {
+    navigate('/Main'); // 메인 페이지로 이동
+  };
   return (
     <div className="login-container">
       <header className="logo-box">
@@ -23,7 +28,7 @@ function Login() {
       <main>
         <p>찐플에 오신걸 환영합니다!</p>
         <h1>넷플릭스 추천작을 한눈에</h1>
-        <button>추천작 보러가기</button>
+        <button onClick={handleBtnClick}>추천작 보러가기</button>
 
       </main>
     </div>
