9b3ec2d439aa5186b110a2abc0fc9af0453cd365
jungjieun <je_eun2735@naver.com>
2023-08-16 19:06:14 +0900

    언어는 한국어와 영어로 설정

--- project/src/hooks/useLanguageToggle.js
+++ project/src/hooks/useLanguageToggle.js
@@ -1,5 +1,10 @@
 import { useState } from "react";
 
 export function useLanguageToggle(){
+  const [language, setLanguage] = useState('ko'); 
 
+  const toggleLanguage = () => {
+    setLanguage(prevLanguage => (prevLanguage === 'ko' ? 'en' : 'ko'));
+  };
+  return { language, toggleLanguage };
 }
