ad2e535678416a9488181822d15c065d16e991bc
jungjieun <je_eun2735@naver.com>
2023-08-16 18:28:23 +0900

    로고 사이즈 수정

--- project/src/pages/Login.tsx
+++ project/src/pages/Login.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import '../styles/login.css';
+import logoImage from '../assets/HJ_logo.png';
 
 function Login() {
   return (
     <div className="login-container">
-      <div className="logo-box"></div>
+      <header className="logo-box">
+        <img src={logoImage} alt='logo'></img>
+        
+        <button>로그인</button>
+      </header>
     </div>
   );
 }
