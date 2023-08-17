8e9c118fe303ba6567816c23f7e34a7364373d8f
jungjieun <je_eun2735@naver.com>
2023-08-17 14:56:46 +0900

    비디오 재생 Fixes #4

--- project/src/pages/Main.tsx
+++ project/src/pages/Main.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import '../styles/main.css';
 import Navbar from '../components/Navbar.tsx';
+import videoSource from '../assets/You.mp4';
+import videoImage from '../assets/You.jpeg';
+
 
 function main() {
   return (
     <div>
       <Navbar />
+      <div class="netflix-video-container">
+        <video src={videoSource} controls poster={videoImage}/>
+      </div>
+
     </div>
   )
 
