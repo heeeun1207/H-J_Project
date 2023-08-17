acde170e5b756edb75e546bcb01670c40534cfb0
jieun <you@example.com>
2023-08-17 18:25:01 +0900

    video의 autoplay 는 muted일때만 가능자동재생 훅이랑 같이 쓰니까 또 안됨

--- project/src/pages/Main.tsx
+++ project/src/pages/Main.tsx
@@ -1,20 +1,23 @@
-import React from 'react';
+
+import React, { useRef } from 'react';
 import '../styles/main.css';
 import Navbar from '../components/Navbar.tsx';
 import videoSource from '../assets/You.mp4';
 import videoImage from '../assets/You.jpeg';
+import useAutoPlayVideo from '../hooks/useAutoPlayVideo';
 
+function Main() {
+  const videoRef = useRef(null);
 
-function main() {
+  useAutoPlayVideo(videoRef);
   return (
     <div>
       <Navbar />
       <div class="netflix-video-container">
-        <video src={videoSource} controls poster={videoImage}/>
+        <video src={videoSource} controls autoPlay muted poster={videoImage}/>
       </div>
 
     </div>
   )
-
   }
-export default main;
+export default Main;
