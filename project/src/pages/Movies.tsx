840c336d8959522bdb8988aa32a5632aa80fcfe0
jungjieun <je_eun2735@naver.com>
2023-08-28 17:52:40 +0900

    테스트 코드 삭제

--- project/src/pages/Movies.tsx
+++ project/src/pages/Movies.tsx
@@ -1,24 +1,13 @@
 import React from 'react';
 import Navbar from '../components/Navbar.tsx';
-import { useMovieData } from '../hooks/MovieDataContext.tsx';
 import CarouselSlider from '../components/CarouselSlider.tsx';
 
 const Movies = () => {
-  const { movies } = useMovieData();
 
 
   return (
     <div>
       <Navbar></Navbar>
-      <div>
-      {movies.map(movie => (
-        <div key={movie.id}>
-          <h2>{movie.title}</h2>
-          <img src={movie.posterUrl} alt={`${movie.title} Poster`} />
-        </div>
-      ))}
-    </div>
-  );
       <CarouselSlider></CarouselSlider>
     </div>
   );
