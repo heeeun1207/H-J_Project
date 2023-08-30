de06b94f5b59b9ac5535e9d40059c3261de1686a
jungjieun <je_eun2735@naver.com>
2023-08-30 17:44:38 +0900

    해당 컴포넌트에만 적용되게 css 클래스

--- project/src/components/CustomModal.tsx
+++ project/src/components/CustomModal.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Modal from 'react-modal';
+import '../styles/CustomModal.css';
 
-const CustomModal = ({ isOpen, closeModal, title, content }) => {
+const CustomModal = ({ isOpen, closeModal, title, content, posterUrl }) => {
 
 
 // function CustomModal({ isOpen, closeModal, title, content }) {
@@ -10,10 +11,13 @@ const CustomModal = ({ isOpen, closeModal, title, content }) => {
       isOpen={isOpen}
       onRequestClose={closeModal}
       contentLabel={title}
+      className="custom-modal" // 스타일 클래스를 적용
+      overlayClassName="custom-modal-overlay" // 스타일 클래스를 적용한 오버레이
     >
+      <img className='modalImg' src={posterUrl} alt={title} />
       <h2>{title}</h2>
       <p>{content}</p>
-      <button onClick={closeModal}>Close</button>
+      <button onClick={closeModal} className="close-button">Close</button>
     </Modal>
   );
 }
