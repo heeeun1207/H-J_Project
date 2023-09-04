7d42391b8ac5e4b7089d06a7f66056154ec6da33
jungjieun <je_eun2735@naver.com>
2023-09-04 13:19:26 +0900

    테스트했던 모달 삭제

--- project/src/pages/Series.tsx
+++ project/src/pages/Series.tsx
@@ -1,30 +1,14 @@
 import React, { useState } from 'react';
-import Navbar from '../components/Navbar.tsx';
-import CustomModal from '../components/CustomModal.tsx';
+import Navbar from '../components/Navbar.tsx'
+import CarouselSlider from '../components/CarouselSlider.tsx';
 
 
 
 const App = () => {
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-
-  const openModal = () => {
-    setModalIsOpen(true);
-  };
-
-  const closeModal = () => {
-    setModalIsOpen(false);
-  };
-
   return (
     <div>
       <Navbar />
-        <button onClick={openModal}>Open Modal</button>
-        <CustomModal
-          isOpen={modalIsOpen}
-          closeModal={closeModal}
-          title="Modal Title"
-          content="This is the content of the modal."
-      />
+      <CarouselSlider></CarouselSlider>
     </div>
   )
   };
