1dcd85034e8cbe23f4268a22b961fe6c1e231132
jungjieun <je_eun2735@naver.com>
2023-09-05 17:59:20 +0900

    내가 찜한 콘텐츠 카드 만들기 끝..!

--- project/src/components/ListCard.tsx
+++ project/src/components/ListCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useWatchList } from '../hooks/WatchListContext.tsx';
+import '../styles/ListCard.css';
 
 const ListCard = () => {
   const { List } = useWatchList();
@@ -8,7 +9,7 @@ const ListCard = () => {
     <div className="watchlist">
       {List.map((item) => (
         <div className="watchlist-card" key={item.id}>
-          <img src={`https://image.tmdb.org/t/p/w500${item.posterUrl}`} alt={item.title} />
+          <img src={`https://image.tmdb.org/t/p/w500${item.posterUrl}`} alt={item.title} className="watchlist-card-img" />
           {/* <h3>{item.title}</h3> */}
           {/* <p>{item.overview}</p> */}
         </div>
