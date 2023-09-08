254162fcec75cee2cb426da192819b208260edb6
jungjieun <je_eun2735@naver.com>
2023-09-08 19:51:40 +0900

    listcard 활용하기

--- project/src/hooks/PopularData.tsx
+++ project/src/hooks/PopularData.tsx
@@ -1,19 +1,21 @@
 import React, { useState, useEffect } from 'react';
+import ListCard from '../components/ListCard.tsx'
 
-const PopularData = () => {
+const PopularData =  ({ tvList, movieList }) => {
   const [tvPopular, setTvPopular] = useState([]);
   const [moviePopular, setMoviePopular] = useState([]);
 
   useEffect(() => {
     const apiKey = '4f7a2baa745822c7e805100300f62cc6';
-    const tvApiUrl = `https://api.theSeriesdb.org/3/tv/popular?api_key=${apiKey}`;
-    const movieApiUrl = `https://api.theSeriesdb.org/3/movie/popular?api_key=${apiKey}`;
+    const tvApiUrl = `https://api.themoviedb.org/3/tv/popular?api_key=${apiKey}`;
+    const movieApiUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`;
 
     // TV 데이터 가져오기
     fetch(tvApiUrl)
       .then(response => response.json())
       .then(data => {
         setTvPopular(data.results);
+        
       })
       .catch(error => {
         console.error('TV 데이터를 불러오는 중 오류 발생:', error);
@@ -32,19 +34,10 @@ const PopularData = () => {
 
   return (
     <div>
-      <h2>인기 TV 시리즈</h2>
-      <ul>
-        {tvPopular.map(show => (
-          <li key={show.id}>{show.name}</li>
-        ))}
-      </ul>
-
-      <h2>인기 영화</h2>
-      <ul>
-        {moviePopular.map(movie => (
-          <li key={movie.id}>{movie.title}</li>
-        ))}
-      </ul>
+< ListCard
+  tvList={tvList}
+  movieList={movieList}
+/>
     </div>
   );
 };
