d79f82a17bc3e9eeedfd593b5e621810b341e7ff
jungjieun <je_eun2735@naver.com>
2023-09-08 20:59:11 +0900

    데이터들은 이 훅에서 가져와서 전달하기

---
+++ project/src/hooks/usePopularData.tsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react';
+
+const usePopularData = () => {
+  const [tvPopular, setTvPopular] = useState([]);
+  const [moviePopular, setMoviePopular] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const apiKey = '4f7a2baa745822c7e805100300f62cc6';
+    const tvApiUrl = `https://api.themoviedb.org/3/tv/popular?api_key=${apiKey}`;
+    const movieApiUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`;
+
+    // TV 데이터 가져오기
+    fetch(tvApiUrl)
+      .then(response => response.json())
+      .then(data => {
+        setTvPopular(data.results);
+      })
+      .catch(error => {
+        console.error('TV 데이터를 불러오는 중 오류 발생:', error);
+      });
+
+    // 영화 데이터 가져오기
+    fetch(movieApiUrl)
+      .then(response => response.json())
+      .then(data => {
+        setMoviePopular(data.results);
+        setLoading(false); 
+      })
+      .catch(error => {
+        console.error('영화 데이터를 불러오는 중 오류 발생:', error);
+      });
+  }, []);
+
+  return { tvPopular, moviePopular, loading };
+};
+
+export default usePopularData;
