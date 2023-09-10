435b44f3bd1eb6eff4b6e7bc1e313a40bea812b5
jungjieun <je_eun2735@naver.com>
2023-09-10 10:59:32 +0900

    검색, 이벤트, 모달 , 스타일 까지!

--- project/src/components/SearchBar.tsx
+++ project/src/components/SearchBar.tsx
@@ -1,6 +1,23 @@
 import React, { useState, useEffect } from 'react';
+import '../styles/SearchBar.css'; 
+import CustomModal from './CustomModal.tsx';
+
 
 const SearchBar = () => {
+
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+
+  const openModal = (item) => { 
+    setSelectedMovie(item);
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setSelectedMovie(null);
+    setModalIsOpen(false);
+  };
+
   const [searchInput, setSearchInput] = useState('');
   const [searchResults, setSearchResults] = useState([]);
   const apiKey = '4f7a2baa745822c7e805100300f62cc6'; 
@@ -27,24 +44,35 @@ const SearchBar = () => {
   }, [searchInput, apiKey]);
 
   return (
-    <div>
-      <input
-        type="text"
-        placeholder="영화, TV 프로그램, 인물 검색"
-        value={searchInput}
-        onChange={(e) => setSearchInput(e.target.value)}
-      />
-      <div>
-        {searchResults.map((item) => (
-          <div key={item.id}>
-            <p>
-              {item.title || item.name} ({item.media_type})
-            </p>
-          </div>
-        ))}
-      </div>
+    <div className="search-bar">
+    <input
+      type="text"
+      className="search-input"
+      placeholder="영화, TV 프로그램, 인물 검색"
+      value={searchInput}
+      onChange={(e) => setSearchInput(e.target.value)}
+    />
+    <div className="search-results">
+      {searchResults.map((item) => (
+        <div className="result-item" key={item.id} onClick={() => openModal(item)}>
+          <img className="img-item" src={`https://image.tmdb.org/t/p/w500${item.poster_path}`} alt={item.title}></img>
+          <p>
+            {item.title || item.name} ({item.media_type})
+          </p>
+        </div>
+      ))}
     </div>
-  );
-};
+    {selectedMovie && (
+        <CustomModal
+          isOpen={modalIsOpen}
+          closeModal={closeModal}
+          posterUrl={`https://image.tmdb.org/t/p/w500${selectedMovie.poster_path}`}
+          title={selectedMovie.title}
+          content={selectedMovie.overview}
+        />
+      )}
+  </div>
 
+);
+};
 export default SearchBar;
